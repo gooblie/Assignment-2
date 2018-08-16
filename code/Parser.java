60e50ef87c9071ae62efab41e8e5da7ea8fd95ee
gooblie <michael42h@gmail.com>
2018-08-16 17:53:03 +1200

    Decided to use stream parser

--- code/Parser.java
+++ code/Parser.java
@@ -1,128 +1,139 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
 
 /**
- * This utility class provides three static methods for parsing each of the
- * three files we're interested in, and returning the relevant data structure.
- * Internally it uses BufferedReaders instead of Scanners to read in the files,
- * as Scanners are pathetically slow.
- * 
- * @author tony
+ * This is the Stream version of the Parser.java file. 
+ * The two files are essentially the same, so feel free to use either of them.
+ * This file is an alternative of Parser.java.
+ * It implements the methods using the Stream functionality.
+ * It is faster, and help you learn a bit of recent and fancy Java functionalities.
  */
 public class Parser {
 
-	public static Map<Integer, Node> parseNodes(File nodes, Graph graph) {
-		Map<Integer, Node> map = new HashMap<Integer, Node>();
+    private Parser(){
+        //empty private constructor
+    }
 
+	private static final Function<String, String[]> splitByTab =
+			line -> line.split("[\t]+");
+
+	public static Map<Integer, Node> parseNodes(
+			File nodes,
+			Graph graph)
+	{
 		try {
-			// make a reader
-			BufferedReader br = new BufferedReader(new FileReader(nodes));
-			String line;
+			final Function<Node, Integer> key = node -> node.nodeID;
+			final Function<Node, Node> value = node -> node;
+			final Function<String[], Node> nodeFrom =
+                    tokens -> {
+                        int nodeID = asInt(tokens[0]);
+                        double lat = asDouble(tokens[1]),
+                               lon = asDouble(tokens[2]);
+                        return new Node(nodeID, lat, lon);
+                    };
 
-			// read in each line of the file
-			while ((line = br.readLine()) != null) {
-				// tokenise the line by splitting it at the tabs.
-				String[] tokens = line.split("[\t]+");
-
-				// process the tokens
-				int nodeID = asInt(tokens[0]);
-				double lat = asDouble(tokens[1]);
-				double lon = asDouble(tokens[2]);
-
-				Node node = new Node(nodeID, lat, lon);
-				map.put(nodeID, node);
-			}
-
-			br.close();
-		} catch (IOException e) {
+			return Files.lines(nodes.toPath())
+						.parallel()
+						.map(splitByTab)
+						.map(nodeFrom)
+                        .collect(Collectors.toMap(key, value));
+		} catch (IOException | NumberFormatException e) {
 			throw new RuntimeException("file reading failed.");
 		}
-
-		return map;
 	}
 
-	public static Map<Integer, Road> parseRoads(File roads, Graph graph) {
-		Map<Integer, Road> map = new HashMap<Integer, Road>();
+	public static Map<Integer, Road> parseRoads(
+			File roads,
+			Graph graph)
+	{
+		try{
+			final Function<Road, Integer> key = road -> road.roadID;
+			final Function<Road, Road> value = road -> road;
+			final Function<String[], Road> toRoad =
+                    tokens -> {
+                        int roadID = asInt(tokens[0]),
+                            type = asInt(tokens[1]);
+                        String label = tokens[2],
+                               city = tokens[3];
+                        int oneway = asInt(tokens[4]),
+                            speed = asInt(tokens[5]),
+                            road_class = asInt(tokens[6]),
+                            not_for_car = asInt(tokens[7]),
+                            not_for_pedestrian = asInt(tokens[8]),
+                            not_for_bicycle = asInt(tokens[9]);
 
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(roads));
-			br.readLine(); // throw away the top line of the file.
-			String line;
+                        return new Road(
+                                roadID, type, label,
+                                city, oneway, speed,
+                                road_class, not_for_car,
+                                not_for_pedestrian, not_for_bicycle
+                        );
+                    };
 
-			while ((line = br.readLine()) != null) {
-				String[] tokens = line.split("[\t]+");
-
-				int roadID = asInt(tokens[0]);
-				int type = asInt(tokens[1]);
-				String label = tokens[2];
-				String city = tokens[3];
-				int oneway = asInt(tokens[4]);
-				int speed = asInt(tokens[5]);
-				int roadclass = asInt(tokens[6]);
-				int notforcar = asInt(tokens[7]);
-				int notforpede = asInt(tokens[8]);
-				int notforbicy = asInt(tokens[8]);
-
-				Road road = new Road(roadID, type, label, city, oneway, speed,
-						roadclass, notforcar, notforpede, notforbicy);
-				map.put(roadID, road);
-			}
-
-			br.close();
-		} catch (IOException e) {
+			return Files.lines(roads.toPath())
+						.skip(1)	//skip header
+						.parallel()
+						.map(splitByTab)
+						.map(toRoad)
+                        .collect(Collectors.toMap(key,value));
+		}catch (IOException | NumberFormatException e){
 			throw new RuntimeException("file reading failed.");
 		}
-
-		return map;
 	}
 
-	public static Collection<Segment> parseSegments(File segments, Graph graph) {
-		Set<Segment> set = new HashSet<Segment>();
+	public static Collection<Segment> parseSegments(
+			File segments,
+			Graph graph)
+	{
+		try{
+		    final Function<String[], Segment> toSegment =
+                    tokens -> {
+                        int roadID = asInt(tokens[0]);
+                        double length = asDouble(tokens[1]);
+                        int node1ID = asInt(tokens[2]),
+                            node2ID = asInt(tokens[3]);
+                        double[] coords = asDouble(tokens,4);
 
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(segments));
-			br.readLine(); // throw away the top line of the file.
-			String line;
+                        return new Segment(
+                                graph, roadID,
+                                length, node1ID,
+                                node2ID, coords
+                        );
+                    };
 
-			while ((line = br.readLine()) != null) {
-				String[] tokens = line.split("[\t]+");
-
-				int roadID = asInt(tokens[0]);
-				double length = asDouble(tokens[1]);
-				int node1ID = asInt(tokens[2]);
-				int node2ID = asInt(tokens[3]);
-
-				double[] coords = new double[tokens.length - 4];
-				for (int i = 4; i < tokens.length; i++)
-					coords[i - 4] = asDouble(tokens[i]);
-
-				Segment segment = new Segment(graph, roadID, length, node1ID,
-						node2ID, coords);
-				set.add(segment);
-			}
-
-			br.close();
-		} catch (IOException e) {
+			return Files.lines(segments.toPath())
+                        .skip(1)	//skip header
+						.parallel()
+                        .map(splitByTab)
+                        .map(toSegment)
+                        .collect(Collectors.toSet());
+		}catch (IOException | NumberFormatException e){
 			throw new RuntimeException("file reading failed.");
 		}
-
-		return set;
 	}
 
-	private static int asInt(String str) {
+	private static int asInt(String str)
+	{
 		return Integer.parseInt(str);
 	}
 
-	private static double asDouble(String str) {
+	private static double asDouble(String str)
+	{
 		return Double.parseDouble(str);
 	}
-}
 
+    private static double[] asDouble(
+            String[] tokens,
+            int skip)
+    {
+        if(skip < 0) skip = 0;
+        return Stream.of(tokens)
+                     .skip(skip)	//skip to read the coordinates
+                     .mapToDouble(Parser::asDouble)
+                     .toArray();
+    }
+}
 // code for COMP261 assignments
