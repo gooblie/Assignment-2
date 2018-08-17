89eb3b12beb56018c42eb0c717fe5e08395d0fdf
gooblie <michael42h@gmail.com>
2018-08-17 15:58:14 +1200

    Added getNeighbors method

--- code/Road.java
+++ code/Road.java
@@ -12,6 +12,8 @@ public class Road {
 	public final int roadID;
 	public final String name, city;
 	public final Collection<Segment> components;
+	public int speed;
+	public boolean oneway;
 
 	public Road(int roadID, int type, String label, String city, int oneway,
 			int speed, int roadclass, int notforcar, int notforpede,
@@ -20,6 +22,8 @@ public class Road {
 		this.city = city;
 		this.name = label;
 		this.components = new HashSet<Segment>();
+		this.speed = speed;
+		this.oneway = oneway == 1;
 	}
 
 	public void addSegment(Segment seg) {
